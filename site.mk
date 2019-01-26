8a5c041588f5e3c3d4793cf7775c879fdf5f42e2
Tarnatos <tarnatos@posteo.de>
2019-01-26 13:47:27 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -2,7 +2,7 @@
 
 # Always call `make` from the command line with the desired release version!
 # otherwise this is generated:
-DEFAULT_GLUON_RELEASE := 2018.2
+DEFAULT_GLUON_RELEASE := 2018.2.0.11
 #DEFAULT_GLUON_RELEASE := 2018.2~exp$(shell date '+%y%m%d')
 
 # Allow overriding the release number from the command line
